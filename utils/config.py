b020e5c9734d9d5b29385f1378fee71381f0b366
Ashutosh <ashutoshsahoo77@yahoo.com>
2026-02-11 17:52:57 +0530

    Change the url to 1234

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 import os
 print("CONFIG FILE PATH:", os.path.abspath(__file__))
 
-Base_Url = "https://fakerestapi.azurewebsites.net/api/v1"
+Base_Url = "https://fakerestapi.azurewebsites.net/api/v1234"
 REQUEST_TIMEOUT = 10
